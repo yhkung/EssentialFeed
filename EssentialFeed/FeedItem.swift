c459912b6a2d053fe3a92d23844c4c44232ccf01
YH Kung <yh@venuslive.com.tw>
2021-05-30 13:56:48 +0800

    Create the FeedItem model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by YH Kung on 2021/5/30.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let location: String?
+    let description: String?
+    let imageURL: URL
+}
