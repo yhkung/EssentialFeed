e38d96a4908bdc2b060d59f2052e0ad3874ca7f2
YH Kung <yh@venuslive.com.tw>
2021-06-03 17:48:31 +0800

    Remove duplicate test code by creating reusable `expect` helper method

--- EssentialFeed/RemoteFeedLoader.swift
+++ EssentialFeed/RemoteFeedLoader.swift
@@ -34,6 +34,7 @@ public final class RemoteFeedLoader {
         client.get(from: url) { result in
             switch result {
             case .success:
+//                break
                 completion(.invalidData)
             case .failure:
                 completion(.connectivity)
