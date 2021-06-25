bd54975d4b903492e7438ee293f56ce6c4563f0c
yhkung <bowser.kung@gmail.com>
2021-06-25 18:57:18 +0800

    Sync whole project files with tutors version

---
+++ EssentialFeed/EssentialFeed/Feed Feature/FeedLoader.swift
@@ -0,0 +1,14 @@
+//
+//  Copyright © 2018 Essential Developer. All rights reserved.
+//
+
+import Foundation
+
+public enum LoadFeedResult {
+	case success([FeedImage])
+	case failure(Error)
+}
+
+public protocol FeedLoader {
+	func load(completion: @escaping (LoadFeedResult) -> Void)
+}
