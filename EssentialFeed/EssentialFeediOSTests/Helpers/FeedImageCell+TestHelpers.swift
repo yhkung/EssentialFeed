c2b1cdb8d4736374b41038113fdb0cad8e6e5236
YHKung <bowser.kung@gmail.com>
2021-08-05 18:38:22 +0800

    Extract FeedImageCell test helpers to a separate file

---
+++ EssentialFeed/EssentialFeediOSTests/Helpers/FeedImageCell+TestHelpers.swift
@@ -0,0 +1,40 @@
+//
+//  FeedImageCell+TestHelpers.swift
+//  EssentialFeediOSTests
+//
+//  Created by YH Kung on 2021/8/5.
+//  Copyright © 2021 Essential Developer. All rights reserved.
+//
+
+import UIKit
+import EssentialFeediOS
+
+extension FeedImageCell {
+    func simulateRetryAction() {
+        feedImageRetryButton.simulateTap()
+    }
+
+    var isShowingLocation: Bool {
+        return !locationContainer.isHidden
+    }
+
+    var isShowingLoadingIndicator: Bool {
+        return feedImageContainer.isShimmering
+    }
+
+    var isShowingRetryAction: Bool {
+        return !feedImageRetryButton.isHidden
+    }
+
+    var locationText: String? {
+        return locationLabel.text
+    }
+
+    var descriptionText: String? {
+        return descriptionLabel.text
+    }
+
+    var renderedImage: Data? {
+        return feedImageView.image?.pngData()
+    }
+}
